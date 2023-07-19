a7ba955192177745e0fe2612eaa63673d0f34bf7
Jonathan Ikpen <jonathanikpen4@gmail.com>
2023-07-19 03:13:34 +0100

    fix: reserved word mode

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 
 interface Mode {
-  mode: "production" | "development";
+  status: "production" | "development";
 }
 
-const CreatePage = ({ mode = "development" }: Mode) => {
+const CreatePage = ({ status = "development" }: Mode) => {
   return (
     <div className="flex flex-col justify-center items-center h-96 w-full p-4 gap-4 ">
       <div className="flex flex-col h-1/3 w-2/3 gap-4 bg-white border rounded justify-center items-center">
         <h1 className="font-bold text-secondary text-3xl">Hola! 👋</h1>
         <h1 className="text-bold text-gray-400 text-sm">We're still cooking</h1>
       </div>
-      {mode === "production" && (
+      {status === "production" && (
         <div className="w-full h-full flex-col md:flex-row gap-10 flex justify-between items-center">
           <Editor />
           <Preview />
